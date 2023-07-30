af10ccac20f3a0b9464166a5e284c5dea342a122
ETR11 <suomalainen.eero@gmail.com>
2023-07-30 21:42:47 +0300

    Commit 4.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useNavigate, Outlet, Route, Routes } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+function BookDetails() {
+  const { bookName } = useParams();
+  const navigate = useNavigate();
+
+  // Fetch the book details based on `bookName`
+
+  return (
+    <div>
+      <h1>Book Details</h1>
+      <p>Name: {bookName}</p>
+      {/* Display other book details */}
+    </div>
+  );
+}
+
+
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>This is not the webpage you are looking for.</p>
+    </div>
+  );
+}
+
 
 function App() {
   const [name, setName] = useState('');
   const [author, setAuthor] = useState('');
   const [pages, setPages] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -31,38 +62,46 @@ function App() {
     } catch (error) {
       console.error('Error:', error);
     }
+    navigate(`/book/${name}`);
   };
 
   return (
-    <div>
-      <h1>books</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Book Name"
-        />
-        <input
-          type="text"
-          id="author"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-          placeholder="Book Author"
-        />
-        <input
-          type="number"
-          id="pages"
-          value={pages}
-          onChange={(e) => setPages(e.target.value)}
-          placeholder="Book Pages"
-        />
-        <button type="submit" id="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+    <Router>
+      <div>
+        <h1>books</h1>
+        <Routes>
+          {/* Other routes */}
+          <Route path="/book/:bookName" element={<BookDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Book Name"
+          />
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            placeholder="Book Author"
+          />
+          <input
+            type="number"
+            id="pages"
+            value={pages}
+            onChange={(e) => setPages(e.target.value)}
+            placeholder="Book Pages"
+          />
+          <button type="submit" id="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Router>
   );
 }
 
